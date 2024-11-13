deee4697a98aa9bbd18ae28d2364bdea976c14a6
James Parrott <japarrott@shaw.ca>
2024-11-13 14:26:08 -0700

    build the modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -4,6 +4,32 @@ const props = defineProps({
 })
 </script>
 
+<template>
+  <Transition name="modal">
+    <div v-if="show" class="modal-mask">
+      <div class="modal-container">
+        <div class="modal-header">
+          <slot name="header">default header</slot>
+        </div>
+
+        <div class="modal-body">
+          <slot name="body">default body</slot>
+        </div>
+
+        <div class="modal-footer">
+          <slot name="footer">
+            default footer
+            <button
+              class="modal-default-button"
+              @click="$emit('close')"
+            >OK</button>
+          </slot>
+        </div>
+      </div>
+    </div>
+  </Transition>
+</template>
+
 <style>
 .modal-mask {
   position: fixed;
